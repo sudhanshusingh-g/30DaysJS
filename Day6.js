750e1d67e86278cfd59b01260c4583c9819af9e8
Sudhanshu Singh <sudsingh32@gmail.com>
2023-11-11 12:56:57 +0530

    Compute Sum

---
+++ Day6.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to compute the sum 
+// of the two given integers.If the two values are the same
+// then return triple their sum.
+function computeSum(a,b){
+    return (a==b)?6*a:a+b
+}
+console.log(computeSum(4,2));//6
+console.log(computeSum(4,4));//24
